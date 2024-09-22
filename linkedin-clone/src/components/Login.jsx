cf2ccb3c4cdabe4078996bb633cf8e3b6a5adb25
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-22 21:45:41 +0100

    add more images

--- linkedin-clone/src/components/Login.jsx
+++ linkedin-clone/src/components/Login.jsx
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
 const Login = (props) => {
-    return <div>Login Page</div>
+    return (
+        <Container>
+            <Nav>
+                <a href='/'>
+                    <img src="../images/login-logo" alt="login-logo" />
+                </a>
+            </Nav>
+        </Container>
+        
+    );
 };
+
+const Container = styled.div ``;
+
+const Nav = styled.div ``;
 
 export default Login;
