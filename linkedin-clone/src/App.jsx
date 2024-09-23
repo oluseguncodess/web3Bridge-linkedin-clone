4f1929d77a161866c975afeb55430ddd201f18f8
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-23 14:19:21 +0100

    done with header

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -1,6 +1,9 @@
-import './App.css';
-import Login from './components/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+
 
 function App() {
   return (
@@ -8,6 +11,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={
+            <>
+              <Header />
+              <Home />
+            </>
+          } />
         </Routes>
       </Router>
     </div>
